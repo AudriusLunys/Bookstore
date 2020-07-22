e37584b5f6a8546b4a6a67cb808c3d7517397b8a
AudriusLunys <lunysaudrius@inbox.lt>
2020-07-22 09:41:40 +0300

    Classwork bookstore with optional table

--- src/main/java/bookstore/main/Main.java
+++ src/main/java/bookstore/main/Main.java
@@ -15,11 +15,15 @@ public class Main {
 
         // creating author
         Author author1 = new Author("Vacys", "Maslionka", LocalDate.of(1988,05,24),"England");
-        Author author2 = new Author("Virgis", "Manka", LocalDate.of(1968,02,12),"Doichland");
+        Author author2 = new Author("Virgis", "Monka", LocalDate.of(1968,02,12),"Germany");
+        Author author3 = new Author("Jonas", "Jonka", LocalDate.of(1956,11,25),"Mali");
+
+
         //adding authors to database
         AuthorDao authorDao = new AuthorDao();
         authorDao.addAuthor(author1);
         authorDao.addAuthor(author2);
+        authorDao.addAuthor(author3);
          // create Bookstore
         Bookstore bookstore1 = new Bookstore("Store of Books", "streetname 4","bookstore@gmail.com");
         Bookstore bookstore2 = new Bookstore("Other Books", "Avenue 4","megastore@yahoo.com");
@@ -27,11 +31,15 @@ public class Main {
         bookstoreDao.addBookStore(bookstore1);
         bookstoreDao.addBookStore(bookstore2);
         // creating book
-        Book book1 = new Book("Zombiai",23.0,author1,bookstore2);
+        Book book1 = new Book("Zombiai",23.0,author1,bookstore1);
         Book book2 = new Book("Betmanas",53.0,author1,bookstore1);
         Book book3 = new Book("Vampyrai",53.50,author1,bookstore1);
-        Book book4 = new Book("Geguzio ziedai",13.90,author2,bookstore1);
-        Book book5 = new Book("Musu sodai",33.50,author2,bookstore2);
+        Book book4 = new Book("Geguzio ziedai",13.90,author2,bookstore2);
+        Book book5 = new Book("Jusu sodai",33.50,author2,bookstore2);
+        Book book6 = new Book("Musu Girios",11.30,author2,bookstore2);
+        Book book7 = new Book("Avengers",123.50,author3,bookstore1);
+        Book book8 = new Book("Ratatuile",11.77,author3,bookstore2);
+        Book book9 = new Book("South",41.50,author3,bookstore2);
 
         //adding authors to database
         BookDao bookDao = new BookDao();
@@ -40,16 +48,26 @@ public class Main {
         bookDao.addBook(book3);
         bookDao.addBook(book4);
         bookDao.addBook(book5);
+        bookDao.addBook(book6);
+        bookDao.addBook(book7);
+        bookDao.addBook(book8);
+        bookDao.addBook(book9);
 
+        // update author
         author1.setCountry("Suvalkai");
         authorDao.updateAuthor(author1);
-
+        // update book
         book2.setPrice(550.0);
         bookDao.updateBook(book2);
+        // update bookstore
+        bookstore1.setAddress("Gedimino pr 5, Vilnius");
+        bookstoreDao.updateBookStore(bookstore1);
 
+        //delete author also deletes his books
         //authorDao.removeAuthor(authorDao.getAuthor(1));
 
-
+        // delete bookstore also deletes books
+        // bookstoreDao.removeBookStore(bookstoreDao.getBookStore(1));
 
     }
 }
